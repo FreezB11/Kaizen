200ebc497e59393ca165cf7f617ea08d7d782ae2
FreezB11 <yashrajrao4@gmail.com>
2024-02-25 19:41:56 +0530

    lol

--- main.c
+++ main.c
@@ -2,15 +2,11 @@
 #include <stdlib.h>
 #include "lib/color.h"
 
-int main (){
-    printf("\a");
+int main (int argc,char* argv[]){
+    printf(RED);
     printf("hello\n");
-    
-    int i = 0;
-    do {
-        printf("%d",i);
-        i++;
-    }while(i<5);
+    printf("%d\n",argc);
+    printf("%s\n",argv[1]);
 
     return 0;
 }
