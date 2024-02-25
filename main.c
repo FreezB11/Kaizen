def3fcbca55b3e08f6aae36eb039b7150abe74cc
FreezB11 <yashrajrao4@gmail.com>
2024-02-25 19:53:07 +0530

    learning about png

--- main.c
+++ main.c
@@ -2,6 +2,12 @@
 #include <stdlib.h>
 #include "lib/color.h"
 
+// enum png {
+//     sign = '89 50 4E 47 0D 0A 1A 0A',
+//     IHDR = '00 00 00 0D 49 48 44 52 00 00 00 01 00 00 00 01 08 02 00 00 00 90 77 53 DE'
+//     IDATA = '',
+// };
+
 int main (int argc,char* argv[]){
     printf(RED);
     printf("hello\n");
