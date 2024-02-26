6bf54660061279d4de9782aea8d4fd2da1531a75
FreezB11 <yashrajrao4@gmail.com>
2024-02-26 12:03:26 +0530

    test

--- main.c
+++ main.c
@@ -1,18 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "lib/color.h"
 
 // enum png {
 //     sign = '89 50 4E 47 0D 0A 1A 0A',
-//     IHDR = '00 00 00 0D 49 48 44 52 00 00 00 01 00 00 00 01 08 02 00 00 00 90 77 53 DE'
-//     IDATA = '',
+//     IHDR = '00 00 00 0D 49 48 44 52 00 00 00 01 00 00 00 01 08 02 00 00 00 90 77 53 DE',
+//     // IDATA = '',
+//     IEND = '00 00 00 00 49 45 4E 44 AE 42 60 82'
 // };
 
 int main (int argc,char* argv[]){
-    printf(RED);
-    printf("hello\n");
-    printf("%d\n",argc);
-    printf("%s\n",argv[1]);
+   
+
+    unsigned char sign[100] = "89 50 4E 47 0D 0A 1A 0A";
+
+    char *input = argv[1];
+    FILE *file = fopen(input,"ab+");
+
+    fwrite(&sign,sizeof(sign),1,file);
+
+    fclose(file);
 
     return 0;
 }
