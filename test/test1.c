85b2bd5028a4fef83c2f383a2fc724800184ad02
FreezB11 <yashrajrao4@gmail.com>
2024-03-09 07:14:16 +0530

    test1 derivate sucess

--- test/test1.c
+++ test/test1.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
  
-double func(float x){
-    return x*x*x*x;
+double func(double x){
+    return x*x;
 }
 
  int main(){
     printf("hello\n");
     double x =2;
-    double eps = 1.61011979e-4;
+    // double eps = 1.61011979e-4;
+    double eps = 1.61012e-4;
     printf("%f\n",eps);
     printf("%f\n",func(x));
   
-    double df = (func(x+eps) -func(x))/eps;
+    double df = (func(x+eps) -func(x-eps))/(2*eps);
     printf("%f\n",df);  
 
     return 0;
