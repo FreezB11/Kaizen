c1e09fac02e1c477c08c9cc0502fbbaa65b54e26
FreezB11 <yashrajrao4@gmail.com>
2024-03-14 04:38:20 +0530

    test fail

--- test/test2.c
+++ test/test2.c
@@ -9,13 +9,15 @@ int main(){
 
     // double eps = 1.61011979e-4;
     double eps = 1.61012e-4;
-    double rate =5e-2 ;
-    double intg;
+    double rate =1.6101e-4 ;
+    double intg= 0;
     printf("rate:%f\n",rate);
-    float out;
-    for (double i = 0; i < 10;i = i+rate)
+    
+    for (double i = 0; i < func(2);i = i+eps)
     {
-        printf("%f\n",i);
+        intg = intg + i*eps;
+        printf("%f\n",intg);
+        //printf("%f\n",i);
     }
     
     
