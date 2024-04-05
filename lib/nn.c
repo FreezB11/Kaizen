99448a0af7b4b6c2ff07188251446afaf170e3a9
FreezB11 <yashrajrao4@gmail.com>
2024-04-05 23:29:07 +0530

    progressing

--- lib/nn.c
+++ lib/nn.c
@@ -1,5 +1,13 @@
 #include "nn.h"
 #include <stdio.h>
+#define RAND_MAX 2147483647
+// nn normalization(float input[]){
+
+// }
+float rand_float(void){
+    return (float)rand()/(float)RAND_MAX;
+}
+
 
 nn LinearREG(float inputs[]){
     // defining weights array based on the size of the input array..
@@ -8,9 +16,12 @@ nn LinearREG(float inputs[]){
     nn_initialize bias;
 
     int summ = 0;
+    for(size_t i=0;i<size;i++){
+        weights[i] = rand_float();
+    }
 
     for(size_t i=0;i<size;i++){
         summ += inputs[i]*weights[i];
     }
-
+    summ += bias;
 }
