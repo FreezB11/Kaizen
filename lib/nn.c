7afaff52498d836009d5a7cfe7b2e0dae6296388
FreezB11 <yashrajrao4@gmail.com>
2024-04-15 20:19:57 +0530

    trying ....

--- lib/nn.c
+++ lib/nn.c
@@ -6,7 +6,7 @@
 
 #define RAND_MAX 2147483647
 
-float eps = 1.61012e-4;
+float eps = 1.61012e-2;
 
 float rand_float(void){
     return (float)rand()/(float)RAND_MAX;
@@ -34,15 +34,51 @@ float cost(int NOS,int NOI,float inputs[NOS][NOI],float output[],float weights[]
 void LinearREG(int NOS,int NOI,float inputs[NOS][NOI],float output[],float weights[]){
     // defining weights array based on the size of the input array..
     float summ = 0.0f;
+    float w1[NOI];
+    float w2[NOI];
     //srand(time(0));
     nn_initialize bias = rand_float();
     printf("bais == %f\n",bias);
     for(int i=0;i<NOI;i++){
         weights[i] = rand_float()*10.0f;
+        w1[i]=weights[i];
         printf("weight[%d]==%f\n",i,weights[i]);
     }
     float c1,c2;
-    float dw1;
     float costt = cost(NOS,NOI,inputs,output,weights,bias);
-    printf("%f\n",costt);
+    printf("initial cost==%f\n",costt);
+ 
+    for (int i = 0; i < NOI; i++)
+    {
+        w1[i] += eps;
+        c1 = cost(NOS,NOI,inputs,output,w1,bias);
+        w1[i] -= eps;
+        printf("c1 == %f\n",c1);
+        w2[i] -= eps;
+        c2 = cost(NOS,NOI,inputs,output,w2,bias); 
+        w2[i] += eps;
+        printf("c2 == %f\n",c2);
+    }
+    
+
+
+
+
+
+
+
+
+
+
+    // for (int i = 0; i < NOI; i++)
+    // {
+    //     weights[i] += eps;
+    //     c1 = cost(NOS,NOI,inputs,output,weights,bias); 
+    //     // weights[i] += 3*eps;
+    //     printf("%f\n",c1);
+    //     weights[i] -= 2*eps;
+    //     c2 = cost(NOS,NOI,inputs,output,weights,bias);
+    //     weights[i] += 2*eps;
+    //     printf("%f\n",c2);
+    // }
 }
