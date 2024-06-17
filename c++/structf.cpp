2521f2a2bce5832bffd0233c4d5b94963b7a18df
FreezB11 <yashrajrao4@gmail.com>
2024-06-18 01:00:26 +0530

    learned classes

--- c++/structf.cpp
+++ c++/structf.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-
+#include <string>
+//testing
 struct tests
 {
     int x;
@@ -7,13 +8,37 @@ struct tests
         return x*x;
     };
 };
+//learning
+class player{
+public:
+    std::string name;
+    float pos_x,pos_y;
+    float speed;
+    void ipp(float x,float y){
+        pos_x = x;
+        pos_y =y;
+    }
+    void move(float dx,float dy){
+        pos_x += dx*speed;
+        pos_y += dy*speed;
+    }
 
+};
 
 int main(){
 
-    struct tests t;
-    t.x = 5;
-    t.sq(5);
-    std::cout << t.sq(5) << std::endl;
+    // struct tests t;
+    // t.x = 5;
+    // t.sq(5);
+    // std::cout << t.sq(5) << std::endl;
+
+    player p;
+    p.name = "adam";
+    p.speed = 0.6;
+    p.ipp(0,0);
+    p.move(5,6);
+
+    std::cout << p.pos_x <<"||"<< p.pos_y << std::endl;
+
     return 0;
 }
