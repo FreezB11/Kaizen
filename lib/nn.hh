66cc24d5e7b47389afb405457d413d87d1a3b72a
FreezB11 <yashrajrao4@gmail.com>
2024-06-29 11:50:57 +0530

    nah man loosing interest on this

--- lib/nn.hh
+++ lib/nn.hh
@@ -5,17 +5,31 @@
 #define log(x) std::cout << x << std::endl;
 #include <iostream>
 
+#define LEARNING_RATE 0.07 // Example learning rate
+#define NUM_EPOCHS 100*1000 // Example number of training epochs
 
-template<typename T>
-class network{
-private:
-    ll layers=0;
-public:
-    network(ll l){
-        layers = l;
-        log(layers)
-    }
-};
+typedef struct {
+    double input;
+    double bias;
+    double output;
+    double delta;
+} neuron;
 
+typedef struct {
+    int num_neurons;
+    neuron *neurons;
+} layer;
+
+typedef struct {
+    int num_layers;
+    layer *layers;
+}network;
+
+network* create_network(int num_layers, int* num_neurons_per_layer);
+void link_layers(network* net);
+void feed_forward(network* net, double* input);
+void backpropagation(network* net, double* target);
+void update_weights(network* net);
+void train_network(network* net, double** inputs, double** targets, int num_training_sets);
 
 #endif
--- main.cc
+++ main.cc
@@ -2,7 +2,7 @@
 #include "lib/nn.hh"
 
 int main(){
-    network<int> nt(5);
+    
 
     return 0;
 }
