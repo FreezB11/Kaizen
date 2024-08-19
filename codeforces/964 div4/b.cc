b7997272bd0f30a27c375f1f165601d0404084c4
FreezB11 <yashrajrao4@gmail.com>
2024-08-19 20:28:35 +0530

    nah bro

--- codeforces/964 div4/b.cc
+++ codeforces/964 div4/b.cc
@@ -3,23 +3,18 @@ using namespace std;
 
 int countSuneetWins(int a1, int a2, int a3, int a4) {
     int suneetWins = 0;
+
     // Consider all permutations of flipping cards:
     // Suneet: (a1, a2) and Slavic: (b1, b2)
     if ((a1 > a3 && a2 > a4)) {
         suneetWins++;
     }
     // Suneet: (a2, a1) and Slavic: (b1, b2)
-    if ((a2 > a3 && a1 > a4)) {
-        suneetWins++;
-    }
-
     if ((a1 > a4 && a2 > a3)) {
         suneetWins++;
     }
 
-    if ((a2 > a4 && a1 > a3)) {
-        suneetWins++;
-    }
+    
     return suneetWins;
 }
 
